c9fc918ca5434a336d72823619e7885249c37d56
Eyal Brami <eyalbrami@gmail.com>
2020-02-16 23:47:44 +0200

    Add project files.

---
+++ L20200216_HomeWork/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L20200216_HomeWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
