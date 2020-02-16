f5384df224d6b0f2581ea6c9cf085b5a1c6c3b2d
Eyal Brami <eyalbrami@gmail.com>
2020-02-17 01:19:50 +0200

    init

--- L20200216_HomeWork/Program.cs
+++ L20200216_HomeWork/Program.cs
@@ -6,10 +6,31 @@ using System.Threading.Tasks;
 
 namespace L20200216_HomeWork
 {
+
+    /*
+     לכתוב מחלקה מלבן שיורשת מ
+     shape
+     שממשת את 
+     getArea
+     ומגדירה מתודה נוספת
+     getPerimeter.
+     אז ליצור מחלקה ריבוע שיורשת ממלבן 
+     ומבלי להגדיר דבר חוץ מקונסטרקטור במחלקה ריבוע 
+     לקבל מחלקה שתעבוד ותחזיר נכון את שטח והיקף הריבוע.
+    */
     class Program
     {
         static void Main(string[] args)
         {
+            Rectangle rectangle = new Rectangle(25, 10);
+            Square square = new Square(10,10); 
+            Console.WriteLine($"the Area of this rectangle is : { rectangle.GetArea() }");
+            Console.WriteLine($"the Perimeter of this rectangle is : { rectangle.GetPerimeter() }");
+
+            Console.WriteLine($"the Area of this square is : { square.GetArea() }");
+            Console.WriteLine($"the Perimeter of this square is : { square.GetPerimeter() }");
+
+            Console.ReadLine();
         }
     }
 }
---
+++ L20200216_HomeWork/Shape.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L20200216_HomeWork
+{
+    public abstract class Shape
+    {
+        public double length { get; set; }
+        public double heigth { get; set; }
+        public abstract double GetArea();
+    }
+    public class Rectangle : Shape
+    {
+        public Rectangle(double length, double heigth)
+        {
+            this.length = length;
+            this.heigth = heigth;
+        }
+        public override double GetArea()
+        {
+            return this.length * this.heigth;
+        }
+        public double GetPerimeter()
+        {
+            return (this.length + this.heigth) * 2;
+        }
+    }
+    public class Square : Rectangle
+    {
+        public Square(double length, double height) : base(length, height) { }
+    }
+}
